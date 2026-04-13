fde7c0dd7b61f5d18d382bef355399416d479c82
MihaitZa10 <mihaicealicu16@gmail.com>
2026-04-13 19:45:46 +0300

    prsima singleton

--- src/server/db.ts
+++ src/server/db.ts
@@ -1,5 +1,16 @@
 import { PrismaClient } from '@prisma/client';
 import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3';
 
-const adapter = new PrismaBetterSqlite3({ url: 'file:./dev.db' });
-export const prisma = new PrismaClient({ adapter });
+function getPrisma() {
+    console.info('Prisma initialized');
+    const adapter = new PrismaBetterSqlite3({ url: 'file:./dev.db' });
+    return new PrismaClient({ adapter });
+}
+
+const globalForPrisma = globalThis as unknown as { prisma: PrismaClient | undefined };
+
+export const prisma = globalForPrisma.prisma ?? getPrisma();
+
+if (process.env.NODE_ENV !== 'production') {
+    globalForPrisma.prisma = prisma;
+}
